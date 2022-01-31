bd86db823c1d32e4907637c5cf3a2f2c6e55819f
Ericlys Lima <ericlyslima05@gmail.com>
2022-01-31 20:15:36 -0300

    Passando Repositorio para GitHub

---
+++ Js/owl/js.js
@@ -0,0 +1,12 @@
+
+function infor(){
+    const bnt = document.getElementById("des");
+    let i = 
+    bnt.innerHTML="Em um futuro próximo, foi lançado um Jogo de Realidade Virtual em Massa para Múltiplos Jogadores Online (VRMMORPG) chamado Sword Art Online, onde seus jogadores controlam seus personagens com o próprio corpo usando um dispositivo tecnológico chamado: NerveGear. Um dia, os jogadores descobrem que não podem sair do jogo, pois o criador do jogo os mantêm presos a menos que eles cheguem ao 100º andar da Torre e derrotem o Boss final. No entanto, se eles morrerem no jogo, morrerão também na vida real. A luta pela sobrevivência começa agora...";
+    bnt
+}
+function offInfor(){
+const bnt2 = document.getElementById("des");
+bnt2.innerHTML = "Em um futuro próximo, foi lançado um Jogo de Realidade Virtual em Massa para Múltiplos Jogadores Online (VRMMORPG) chamado Sword Art Online, onde seus jogadores controlam seus personagens...";
+bnt2.addEventListener('mousedown', offInfor());
+}
